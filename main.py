99ef441db3a88bbd69f2aaef892de92816cbd2ae
peternemeth05 <pnemethasd@gmail.com>
2025-07-27 16:14:32 +0100

    added example data and almost finished main

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 import requests
+import xml.etree.ElementTree as ET # ElementTree is a builtin py library
+
+from time import time 
 
 def get_station_availability(station_name: str) -> str:
 
     XML_URL = "https://tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml"
+    # XML is formatted like a family tree
+    #   -every document has one 'root' element (single ancestor from which everything branches)
+    #   - our root element is <stations> -> <station> -> <'station characterstics' eg nbBikes, nbDocks> &
 
     try:
         response = requests.get(XML_URL, timeout = 10)
+        response.raise_for_status()
 
     except requests.exceptions.RequestException as e:
         return f"Error fetching data: {e}"
+    
 
-    print(response.content.strip())
+    root = ET.fromstring(response.content)
+
+    for station in root.findall('station'):
+        name = station.find('name').text
+
+        if name == station_name:
+            total_bikes = station.find('nbBikes').text
+            empty_docks = station.find('nbEmptyDocks').text
+            ebikes = station.find('nbEBikes').text
+            standard_bikes = station.find('nbStandardBikes').text
+
+
+    print(f"\ntotal bikes is {total_bikes}, with {empty_docks} empty docks\n")
+    #print((response.content[0:100]))
 
 
 if __name__ == "__main__":
+    t1 = time()
 
     get_station_availability("River Street , Clerkenwell")
+
+    print(f"time for first station data {time()-t1}\n")
+
+    t2 = time()
+
+    get_station_availability("Victoria & Albert Museum, South Kensington")
+
+    print(f"time for last station data {time()-t2}\n")
 
 
     pass
